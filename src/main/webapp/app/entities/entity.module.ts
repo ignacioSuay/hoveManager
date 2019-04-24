cde77ecfab9805a8123deaad2801f7be53787245
ignacio suay <ignacio.suay@gmail.com>
2019-04-24 18:23:13 +0100

    Author and jenkins

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,10 @@ import { RouterModule } from '@angular/router';
 @NgModule({
     imports: [
         RouterModule.forChild([
+            {
+                path: 'author',
+                loadChildren: './author/author.module#HoveManagerAuthorModule'
+            }
             /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
         ])
     ],
